9fbd9c9aac362d15e5a75ca0b567e0431933440a
Nick Parsons <nparsons08@gmail.com>
2014-05-18 23:07:43 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var url = require('url');
+
+module.exports = function (opts) {
+
+    opts = opts || {};
+
+    var https = function(req, res, next) {
+
+        if (opts.override) {
+            return next();
+        }
+
+        if (req.headers['x-forwarded-proto'] != 'https') {
+            return next(new restify.NotAuthorizedError('An HTTPS connection is required to request this resource.'));
+        }
+
+        return next();
+
+    };
+
+    return (https);
+
+};
